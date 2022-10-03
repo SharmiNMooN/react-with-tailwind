29ced2cf2754d64ad760eaa883307209d30ce33a
Sharmin Moon <sharminmoon0308@gmail.com>
2022-10-03 21:14:30 +0600

    added Nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className="text-6xl underline">This ia a big big header</h1>
       <p>This is just a paragraph</p>
     </div>
